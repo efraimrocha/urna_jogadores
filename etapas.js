1f1c7673f22f375335c82faf3a0e2ad0b87e71b7
Efraim Rocha <efraimrocha86@gmail.com>
2022-12-07 22:07:04 -0300

    Finalização da urna - Faltando add o som de finalização

---
+++ etapas.js
@@ -0,0 +1,70 @@
+let etapas = [
+  {
+      titulo: 'GOLEIRO',
+      numeros: 3,
+      candidatos: [
+          {
+              numero: '111',
+              nome: 'Alison Becker',
+              pais: 'Brail',
+              fotos:[
+                  {url:'alison.jpg', posicao: 'Defesa'}
+              ]
+          },
+          {
+              numero: '222',
+              nome: 'Ochoa',
+              pais: 'México',
+              fotos:[
+                  {url:'ochoa.jpg', posicao: 'Defesa'}
+              ]
+          },
+          {
+            numero: '333',
+            nome: 'Emiliano Martinez',
+            pais: 'Argentina',
+            fotos:[
+                {url:'emilianoMartinez.jpg', posicao: 'Defesa'}
+            ]
+        },
+      ]
+  },
+  {
+      titulo: 'JOGADOR',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: '13',
+              nome: 'Casimiro',
+              pais: 'Brasil',
+              fotos:[
+                  {url:'casimiro.jpg', posicao: 'Atacante'},
+              ]
+          },
+          {
+              numero: '22',
+              nome: 'Neymar',
+              pais: 'Brasil',
+              fotos:[
+                  {url:'neymar.jpg', posicao: 'Atacante'},
+              ]
+          },
+          {
+            numero: '33',
+            nome: 'Cristiano Ronaldo',
+            pais: 'Portugal',
+            fotos:[
+                {url:'cristiano.jpg', posicao: 'Atacante'},
+            ]
+        },
+        {
+            numero: '11',
+            nome: 'Messi',
+            pais: 'Argentina',
+            fotos:[
+                {url:'messi.jpg', posicao: 'Atacante'},
+            ]
+        },
+      ]
+  }
+];
